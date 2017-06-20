aa43d449d3c02ca741faa6e07ea6ad110499d4bb
阿浩 <ah@AHdeMacBook-Pro.local>
2017-06-20 10:49:39 +0800

    Initial Commit

---
+++ AHPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AHPlayer
+//
+//  Created by AH on 2017/6/20.
+//  Copyright © 2017年 AH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AHPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AHPlayer
+//
+//  Created by AH on 2017/6/20.
+//  Copyright © 2017年 AH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
